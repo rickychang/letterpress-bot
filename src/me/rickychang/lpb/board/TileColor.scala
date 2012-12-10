b24009cab09c69aacb14e5369cce3a22083da539
Ricky Chang <rchang1@gmail.com>
2012-12-10 08:33:46 -0500

    field renaming

--- src/me/rickychang/lpb/board/TileColor.scala
+++ src/me/rickychang/lpb/board/TileColor.scala
@@ -4,12 +4,12 @@ package me.rickychang.lpb.board
  * Representation of Tile Colors (States).  Used for determining current player and opponent scores and
  * evaluating potential words to be played.
  */
-sealed abstract class TileColor(name: String, shortName: String, currrentVal: Short, playerPotential: Short, opponentPotential: Short) {
+sealed abstract class TileColor(description: String, shortName: String, currrentVal: Short, playerPotential: Short, opponentPotential: Short) {
   override def toString: String = shortName
 }
 
 case object Free extends TileColor("Free", "w", 0, 1, 0)
 case object PlayerOccupied extends TileColor("Player Occupied", "b", 1, 0, 0)
 case object PlayerDefended extends TileColor("Player Defended", "B", 1, 0, 0)
-case object OpponentOccupied extends TileColor("OpponentOccupied", "r", -1, 1, -1)
-case object OpponentDefended extends TileColor("OpponentDefended", "R", -1, 0, 0)
+case object OpponentOccupied extends TileColor("Opponent Occupied", "r", -1, 1, -1)
+case object OpponentDefended extends TileColor("Opponent Defended", "R", -1, 0, 0)
