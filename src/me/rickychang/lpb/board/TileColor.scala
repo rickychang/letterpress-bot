1370727690fd069bcbf78e7a6b6e748b2362ee3b
Ricky Chang <rchang1@gmail.com>
2012-12-10 08:32:35 -0500

    adding shortname field to TileColor for debugging

--- src/me/rickychang/lpb/board/TileColor.scala
+++ src/me/rickychang/lpb/board/TileColor.scala
@@ -4,10 +4,12 @@ package me.rickychang.lpb.board
  * Representation of Tile Colors (States).  Used for determining current player and opponent scores and
  * evaluating potential words to be played.
  */
-sealed abstract class TileColor(name: String, currrentVal: Short, playerPotential: Short, opponentPotential: Short)
+sealed abstract class TileColor(name: String, shortName: String, currrentVal: Short, playerPotential: Short, opponentPotential: Short) {
+  override def toString: String = shortName
+}
 
-case object Free extends TileColor("Free", 0, 1, 0)
-case object PlayerOccupied extends TileColor("Player Occupied", 1, 0, 0)
-case object PlayerDefended extends TileColor("Player Defended", 1, 0, 0)
-case object OpponentOccupied extends TileColor("OpponentOccupied", -1, 1, -1)
-case object OpponentDefended extends TileColor("OpponentDefended", -1, 0, 0)
+case object Free extends TileColor("Free", "w", 0, 1, 0)
+case object PlayerOccupied extends TileColor("Player Occupied", "b", 1, 0, 0)
+case object PlayerDefended extends TileColor("Player Defended", "B", 1, 0, 0)
+case object OpponentOccupied extends TileColor("OpponentOccupied", "r", -1, 1, -1)
+case object OpponentDefended extends TileColor("OpponentDefended", "R", -1, 0, 0)
