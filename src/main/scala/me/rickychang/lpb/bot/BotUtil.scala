c401437b07b3f445ea1a483ea3399b97d337707b
Ricky Chang <rchang1@gmail.com>
2013-01-08 22:37:31 -0500

    adding comment to truncateTweet method

--- src/main/scala/me/rickychang/lpb/bot/BotUtil.scala
+++ src/main/scala/me/rickychang/lpb/bot/BotUtil.scala
@@ -1,8 +1,12 @@
 package me.rickychang.lpb.bot
 
 object BotUtil {
+
   def truncateTweet(tweetText: String) = {
     if (tweetText.length <= 140) tweetText
+    // if tweet is too long, truncate and append ellipsis
+    // unicode character
     else tweetText.substring(0, 140) + "\u2026"
   }
+
 }
