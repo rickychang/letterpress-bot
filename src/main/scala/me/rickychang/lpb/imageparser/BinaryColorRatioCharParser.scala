8d0110014fdb1e1857298963283fd3a2d20d9831
Ricky Chang <rchang1@gmail.com>
2013-01-20 16:22:28 -0500

    adding empty char parser class

---
+++ src/main/scala/me/rickychang/lpb/imageparser/BinaryColorRatioCharParser.scala
@@ -0,0 +1,11 @@
+package me.rickychang.lpb.imageparser
+
+import java.awt.image.BufferedImage
+
+class BinaryColorRatioCharParser extends TileCharParser {
+
+  def extractChar(tileImage: BufferedImage): Char = { 
+    Char.MinValue 
+  }
+
+}
